16fed747bd22eeeb85e23190d254e73179a7302f
Miriam Stewart <MiriamAStewart@yahoo.com>
2021-02-17 04:01:15 -0500

    Initial commit

---
+++ src/main/java/com/restful/restful_application/RestfulApplication.java
@@ -0,0 +1,14 @@
+package com.restful.restful_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RestfulApplication.class, args);
+        System.out.println("Testing github");
+    }
+}
